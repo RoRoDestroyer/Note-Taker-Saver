ce07a5c861fd7019239fd7ff258f1a120ef7d907
Rowan <73924951+RoRoDestroyer@users.noreply.github.com>
2023-09-13 21:16:29 -0400

    Update noteTAS.cs

--- noteTAS.cs
+++ noteTAS.cs
@@ -1,27 +1,31 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        private static string note = ""; 
+        private static string note = ""; // Declare note at the class level.
 
         static void Main(string[] args)
         {
             Console.Write("Write note: ");
-            note = GetSecureInput(); 
+            note = GetSecureInput(); // Assign the value to the class-level note variable.
 
             Console.Clear();
 
-            Console.Write("Note Taken! Press Enter to view your note!");
-            Console.ReadLine();
-
-            Console.WriteLine(note + " Press Enter to exit or press S to save to files!");
+            Console.Write("Note Taken! Press Enter to view your note or press S to save and open the file after saving: ");
             string key = Console.ReadLine();
-            if (key != null && (key.ToUpper() == "S" || key.ToUpper() == "S"))
+
+            if (key != null && key.ToUpper() == "S")
             {
                 SaveToFile(note);
+            }
+            else
+            {
+                Console.WriteLine(note + " | Press Enter to exit.");
+                Console.ReadLine();
             }
         }
 
@@ -51,11 +55,21 @@ namespace ConsoleApp1
 
         static void SaveToFile(string content)
         {
-            string filePath = @"C:\Users..."; //put path to txt document
+            string filePath = @"C:\Users\rags2\Downloads\notes.txt"; // Put path to txt document
 
             File.WriteAllText(filePath, content);
 
-            Console.WriteLine("Note saved to file successfully");
+            Console.WriteLine("String saved to file successfully!");
+
+            Console.Write("Do you want to open the file? (Y/N): ");
+            string openChoice = Console.ReadLine();
+
+            if (openChoice != null && openChoice.ToUpper() == "Y")
+            {
+                // Open the saved file with the default associated program (e.g., Notepad).
+                Process.Start(filePath);
+            }
+
             Console.ReadLine();
         }
     }
